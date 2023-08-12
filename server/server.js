575b0d891c2f35f570b11ee51df8ec1bc6b4d538
YoussefRs <rs.youssef24@gmail.com>
2023-08-12 01:25:11 +0100

    server_setup

---
+++ server/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+// const fileUpload = require('express-fileupload');
+const errorMiddleware = require('./middleswares/error');
+
+const app = express();
+
+//config
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended : true }));
+// app.use(fileUpload());
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Server is Running! 🚀');
+});
+
+//error middleware
+app.use(errorMiddleware);
+
+module.exports = app;
