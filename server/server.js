1ec2d86ebdf17c57352d237189451592e6eecd0f
YoussefRs <rs.youssef24@gmail.com>
2023-08-13 01:01:09 +0100

    'front_done'

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 // const fileUpload = require('express-fileupload');
 const errorMiddleware = require('./middleswares/error');
+const authRoute = require('./routes/authRoutes');
 
 const app = express();
 
@@ -16,6 +17,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended : true }));
 // app.use(fileUpload());
+
+app.use('/api/user', authRoute);
 
 
 
